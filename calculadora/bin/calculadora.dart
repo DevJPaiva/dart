b342b9b7d399254eaf417708e54e9654db82bf04
DevJPaiva <jorgefe0210@gmail.com>
2025-11-04 19:33:35 -0300

    Add user input and sum calculation to main
    Main function now reads two numbers from stdin, prints their sum, and defines a 'somar' function to print the sum again. This enhances the calculator to perform addition based on user input.

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
+
 void main() {
-  print('oi');
+  double n1 = double.parse(stdin.readLineSync()!);
+  double n2 = double.parse(stdin.readLineSync()!);
+
+  print(n1 + n2);
+
+  void somar() {
+    print(n1 + n2);
+  }
+
+  somar();
 }
