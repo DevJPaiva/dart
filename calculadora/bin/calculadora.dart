fdb5de927668ba099cba796c2206c44ba5279709
DevJPaiva <jorgefe0210@gmail.com>
2025-11-04 18:20:27 -0300

    Simplify main function and output message
    Removed unused import and arguments from main. Changed printed message from calculation result to a simple greeting.

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -1,5 +1,3 @@
-import 'package:calculadora/calculadora.dart' as calculadora;
-
-void main(List<String> arguments) {
-  print('Hello world: ${calculadora.calculate()}!');
+void main() {
+  print('oi');
 }
