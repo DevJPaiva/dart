1bdd025c1d098bb83ce649525c6ac62012760356
DevJPaiva <jorgefe0210@gmail.com>
2025-11-04 20:03:22 -0300

    Add subtraction, division, and multiplication functions
    Expanded the calculator to include subtrair, dividir, and multiplicar functions, allowing basic arithmetic operations beyond addition.

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -4,11 +4,24 @@ void main() {
   double n1 = double.parse(stdin.readLineSync()!);
   double n2 = double.parse(stdin.readLineSync()!);
 
-  print(n1 + n2);
-
   void somar() {
     print(n1 + n2);
   }
 
+  void subtrair() {
+    print(n1 - n2);
+  }
+
+  void dividir() {
+    print(n1 / n2);
+  }
+
+  void multiplicar() {
+    print(n1 * n2);
+  }
+
   somar();
+  subtrair();
+  dividir();
+  multiplicar();
 }
