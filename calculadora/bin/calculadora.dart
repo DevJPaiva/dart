82b934ccb73bfbf2203adebbe363dfc2f1c18da1
DevJPaiva <jorgefe0210@gmail.com>
2025-11-04 21:39:03 -0300

    Add user input for operation selection
    The calculator now prompts the user to enter two numbers and select an operation (+, -, *, /). The chosen operation is executed using a switch statement, improving usability and interaction.

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
 
 void main() {
+  print('Digite o primeiro número:');
   double n1 = double.parse(stdin.readLineSync()!);
+
+  print('Digite o segundo número:');
   double n2 = double.parse(stdin.readLineSync()!);
+
+  print('Digite a operação (+, -, *, /):');
+  String operacao = stdin.readLineSync()!.trim();
 
   void somar() {
     print(n1 + n2);
@@ -20,8 +26,25 @@ void main() {
     print(n1 * n2);
   }
 
-  somar();
-  subtrair();
-  dividir();
-  multiplicar();
+  switch (operacao) {
+    case '+':
+      somar();
+      break;
+
+    case '-':
+      subtrair();
+      break;
+
+    case '*':
+      multiplicar();
+      break;
+
+    case '/':
+      dividir();
+      break;
+
+    default:
+      print('Operação inválida. Use +, -, * ou /.');
+      break;
+  }
 }
